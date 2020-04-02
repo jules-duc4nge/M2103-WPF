d3dccea15627190d8d2aa848c11664a609356a0a
Jules <jules.ducange@etud.u-picardie.fr>
2020-04-02 16:11:43 +0200

    Implemant DrawingSurface class

--- Tp-02/Tp-02/BKP_MainWindow.xaml.cs
+++ Tp-02/Tp-02/BKP_MainWindow.xaml.cs
@@ -57,16 +57,67 @@ namespace Tp_02
     }
     public partial class MainWindow : Window
     {
+        List<Point> coordinates = new List<Point>(2);
         List<SolidColorBrush> colors = 
             new List<SolidColorBrush> {Brushes.Orange, Brushes.Green, Brushes.Blue};
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        /*private void Cvs_Draw_Click(object sender, MouseButtonEventArgs e)
+        {
+            if (coordinates.Count==0)
+            {
+                coordinates.Add(Mouse.GetPosition(Cvs_Draw));
+            } else
+            {
+                coordinates.Add(Mouse.GetPosition(Cvs_Draw));
+                Line line = new Line
+                {
+                    X1 = coordinates[0].X,
+                    Y1 = coordinates[0].Y,
+                    X2 = coordinates[1].X,
+                    Y2 = coordinates[1].Y,
+                    Stroke = colors[Cbb_Colors.SelectedIndex]
+                };
+                Cvs_Draw.Children.Add(line);
+                coordinates.RemoveAt(0);
+            }
+        }*/
+        
+        private void Cvs_Draw_Click(object sender, MouseButtonEventArgs e)
+        {
+ 
+            /*
+            if (double.IsNaN(Cvs_Draw.last_touch.X))
+            {
+                Cvs_Draw.last_touch = Mouse.GetPosition(Cvs_Draw);
+                //coordinates.Add(Mouse.GetPosition(Cvs_Draw));
+            }
+            else
+            {
+                coordinates.Add(Mouse.GetPosition(Cvs_Draw));
+                Line line = new Line
+                {
+                    X1 = coordinates[0].X,
+                    Y1 = coordinates[0].Y,
+                    X2 = coordinates[1].X,
+                    Y2 = coordinates[1].Y,
+                    Stroke = colors[Cbb_Colors.SelectedIndex]
+                };
+                Cvs_Draw.Children.Add(line);
+                coordinates.RemoveAt(0);
+                Cvs_Draw.Add_Stroke(colors[Cbb_Colors.SelectedIndex]);
+            }
+            */
+        }
         private void Btn_clear_Click(object sender, RoutedEventArgs e)
         {
             Cvs_Draw.Reset();
+        //coordinates.RemoveAt(0);
         }
+
         private void Cbb_Colors_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Cvs_Draw.current_color = colors[Cbb_Colors.SelectedIndex];
